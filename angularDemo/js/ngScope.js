445962acc9f24c5e5ebefe67a155ba9947b4658f
ThingsChange <564777909@qq.com>
2016-12-14 10:03:54 +0800

    1

--- angularDemo/js/ngScope.js
+++ angularDemo/js/ngScope.js
@@ -1,3 +1,19 @@
 /**
  * Created by wanglijun on 2016/12/14.
  */
+angular.module('myApp', [])
+    .controller('SomeController', function ($scope) {
+        // 反模式，裸值
+        $scope.someBareValue = 'hello computer';
+// 设置 $scope 本身的操作，这样没问题
+        $scope.someAction = function () {
+// 在SomeController和ChildController中设置{{ someBareValue }}
+            $scope.someBareValue = 'hello human, from parent';
+        };
+    })
+    .controller('ChildController', function ($scope) {
+        $scope.childAction = function () {
+// 在ChildController中设置{{ someBareValue }}
+            $scope.someBareValue = 'hello human, from child';
+        };
+    });
