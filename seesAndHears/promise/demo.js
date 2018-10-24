dafd3f2378249ed11363626247cd2d476c7c9642
qingyun <564777909@qq.com>
2018-10-24 23:31:12 +0800

    什么鬼玩意儿

---
+++ seesAndHears/promise/demo.js
@@ -0,0 +1,69 @@
+// https://www.jb51.net/article/124321.htm
+/* 解释可以参考*/
+async function testSometing() {
+  console.log('执行testSometing')
+  return 'testSometing'
+}
+
+async function testAsync() {
+  console.log('执行testAsync')
+  return Promise.resolve('hello async')
+}
+async function testV3() {
+  console.log('testV3')
+  return Promise.resolve('hello testV3')
+}
+
+async function test() {
+  console.log('test start...')
+  const v1 = await testSometing()
+  console.log(v1)
+  const v2 = await testAsync()
+  console.log(v2)
+  console.log(v1, v2)
+  const v3 = await testV3()
+  console.log(v3)
+}
+
+test()
+
+var promise = new Promise((resolve) => {
+  console.log('promise start..')
+  resolve('promise')
+})// 3
+promise.then((val) => {
+  console.log(val)
+  return val
+}).then(function(res) {
+  console.log(res)
+  return 3
+}).then(function(res) {
+  console.log(res)
+  return 4
+}).then(function(res) {
+  console.log(res)
+  return 5
+}).then(function(res) {
+  console.log(res)
+  return 6
+}).then(function(res) {
+  console.log(res)
+  return 7
+}).then(function(res) {
+  console.log(res)
+  return 8
+}).then(function(res) {
+  console.log(res)
+  return 9
+}).then(function(res) {
+  console.log(res)
+  return 10
+}).then(function(res) {
+  console.log(res)
+  return 11
+}).then(function(res) {
+  console.log(res)
+  return 12
+})
+
+console.log('test end...')
