07d49079bf43cadc5b50fb53e90d155869ca026a
晴云 <564777909@qq.com>
2021-05-30 15:34:10 +0800

    jira填写工时

---
+++ test.js
@@ -0,0 +1,106 @@
+1  {}
+
+this;
+
+Object.prototype.toString.call(obj).slice(8,-1) ==='[Object Array]'
+a instanceof Array
+
+$resolve
+reject
+all  [,proomise,p]
+race
+allSet
+Catch.finally
+then
+
+
+PromiseAll=function (params{
+  let result =[];
+  params.map().forEach(){
+
+
+    result;''
+  }
+}
+
+
+
+基本数据
+dep
+watcher
+Observer
+compiler
+debugger
+
+this.$nextTick(()=>{
+
+}))
+this.a=1;
+
+JS事件队列
+
+微任务
+
+宏任务
+
+
+
+
+
+
+
+
+
+引用数据类型
+
+
+add(1)(2,3)(3,4,5)()
+
+function add(...params){
+ let inNeParams=[];
+ let sum= function (){
+   if(arguments.length===0){
+     return inNeParams.reduce((vv ,v) => {
+       return vv+v;
+     },0 )
+   }else{
+     inNeParams=[...inNeParams,...params];
+     return sum
+   }
+ }
+ return sum;
+}
+
+
+箭头函数
+
+
+不能改变this
+不能ge
+不能  构造函数
+
+没有arguments
+
+a();
+new a();
+call  apply  bind
+
+b.a   b[a]   b;
+window;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
