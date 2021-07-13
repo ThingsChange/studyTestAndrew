37dfcc2330bae4dffb3a683f74cd429fd47f6c4b
wanglijun <wanglijun@hualala.com>
2021-07-13 08:51:53 +0800

    尝试

--- someTimes/1.js
+++ someTimes/1.js
@@ -1,4 +1,3 @@
-
 /**
  *
  * @author  晴云
@@ -6,13 +5,13 @@
  * @note 干什么的呢？
  **/
 
-let b=[3,0,7];
-let a=[5,0];
+let b = [3, 0, 7];
+let a = [5, 0];
 //53271
-let ans=[]
-while(a.length && b.length){
-    if(a > b)  ans.push(a.shift())
+let ans = []
+while (a.length && b.length) {
+  if (a > b) ans.push(a.shift())
   else ans.push(b.shift())
 }
-ans=ans.concat(a,b);
+ans = ans.concat(a, b);
 console.log(ans)
